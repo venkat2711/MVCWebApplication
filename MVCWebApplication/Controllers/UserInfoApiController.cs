b6e9413482445645e8026d0c92ce056d89a879a9
VE843462 <VE843462@L-156153889>
2018-04-19 16:58:59 +0530

    Add project files.

---
+++ MVCWebApplication/Controllers/UserInfoApiController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EntityLayer;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVCWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserInfoApiController : Controller
+    {
+        [Route("GetUserDetails")]
+        [HttpGet]
+        public List<UserDetails> GetUserDetails()
+        {
+            List<UserDetails> lstuserDetails = new List<UserDetails>();
+            UserDetails objuserDetails = new UserDetails();
+            objuserDetails.UserId = 1;
+            objuserDetails.UserName = "Venkatgiri";
+            objuserDetails.EmailId = "venkatgiri.sridharan1@wipro.com";
+            objuserDetails.MobileNumber = "9884443626";
+            lstuserDetails.Add(objuserDetails);
+            objuserDetails = new UserDetails();
+            objuserDetails.UserId = 2;
+            objuserDetails.UserName = "Bagyaraj";
+            objuserDetails.EmailId = "bagyaraj.s09@wipro.com";
+            objuserDetails.MobileNumber = "9962533456";
+            lstuserDetails.Add(objuserDetails);
+            objuserDetails = new UserDetails();
+            objuserDetails.UserId = 3;
+            objuserDetails.UserName = "Shivam Srivastava";
+            objuserDetails.EmailId = "shivam.srivastava4@wipro.com";
+            objuserDetails.MobileNumber = "8510888769";
+            lstuserDetails.Add(objuserDetails);
+            return lstuserDetails;
+
+        }
+    }
+}
---
+++ MVCWebApplication/wwwroot/js/User.js
@@ -0,0 +1,45 @@
+﻿var tableNo = 1;
+var Url = 'http://localhost:63941/api/';
+$(document).ready(function () {
+    BindUserdetails()
+});
+
+function BindUserdetails() {
+    var rowData = '';
+    var Userdetailsurl = Url + "UserInfoApi/GetUserDetails";
+    $.ajax({
+        type: "GET",
+        url: Userdetailsurl,
+        async: true,
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        Accept: "application/json; charset=utf-8",
+        crossDomain: true,
+        success: function (data) {
+            if (data.length > 0) {
+                $("#noDataDiv").hide();
+                $("#dataDiv").show();
+                $.each(data, function (key, value) {
+                    var id = key + 1
+                    rowData += "<tr>";
+                    rowData += '<td class="Grid-td">' + value.userId + "</td>";
+                    rowData += '<td class="Grid-td">' + value.userName + '</td><td class="Grid-td">' + value.emailId + '</td><td class="Grid-td">' + value.mobileNumber + '</td>';
+                });
+                var tableHeader = "<table id='example" + tableNo.toString() + "' class='display' width='100%'><thead><tr>";
+                tableHeader += "<th class='grid-header'>User Id</th><th class='grid-header'>Name</th><th class='grid-header'>Email Id</th><th class='grid-header'>Mobile No</th>";
+                tableHeader += "</tr></thead><tbody>";
+                $("#tableDiv").html(tableHeader + rowData + '</tr></tbody></table>');
+
+                tableNo++;
+            }
+            else {
+                $("#lblNoData").html("No data found");
+                $("#noDataDiv").hide();
+                $("#dataDiv").hide();
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert("Error occurred while fetching project details" + "\n" + jqXHR + "\n" + textStatus + "\n" + errorThrown);
+        }
+    });
+}
